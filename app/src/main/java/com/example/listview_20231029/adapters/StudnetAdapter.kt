351d1f74bce9e25f828388c4fc2e78fcc13ba294
mnyu123 <whdudwo5582@gmail.com>
2023-10-29 13:16:57 +0900

    리스트 뷰 어댑터 실제데이터와 연동

--- app/src/main/java/com/example/listview_20231029/adapters/StudnetAdapter.kt
+++ app/src/main/java/com/example/listview_20231029/adapters/StudnetAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.example.listview_20231029.R
 import com.example.listview_20231029.datas.StudnetData
 
@@ -16,9 +17,9 @@ class StudnetAdapter(
     // 어댑터 클래스가 생성자에게 부모(studentData)에게서 받아오게
 
     // ListView 커스텀 작업도 여기서 한다.
-    mContext : Context,
-    resID : Int,
-    mList : ArrayList<StudnetData>
+    val mContext : Context,
+    val resID : Int,
+    val mList : ArrayList<StudnetData>
 ) : ArrayAdapter<StudnetData>(mContext,resID,mList) {
     // 커스텀으로 만든 xml을 -> 코틀린에서 끌어와주는 객체 작성
     val inf = LayoutInflater.from(mContext)
@@ -32,8 +33,27 @@ class StudnetAdapter(
                                                    // 레이아웃 ,  부가정보
             tempRow = inf.inflate(R.layout.student_list_item , null)
         }
-
+        // row = xml을 그려낸 객체 변수
         val row = tempRow!!
+
+        // xml을 그린 row 객체 내부에서 , TextView등 같은걸 찾아서 실제 데이터로 연동하는것
+        // 실제 데이터 = 어떤 학생 , mList의 position에 맞는 객체
+
+        val stdData = mList[position] // 그림을 그려줄 위치에 맞는 학생 객체 추출
+
+        // row안에 이름 표시는 TextView를 변수로 가져와야 , stdData의 이름값을 대입가능
+        // 어탭터 안에서는 데이터 바인딩 사용불가!!
+        // 직접 코드로 불러와야함(텍스트 뷰들 아이디 같은거)
+
+        val txtname = row.findViewById<TextView>(R.id.txtName)
+        val txtPhoneNum = row.findViewById<TextView>(R.id.txtPhoneNum)
+        val txtAge = row.findViewById<TextView>(R.id.txtAge)
+
+        txtname.text = stdData.name
+        txtPhoneNum.text = stdData.phoneNum
+
+        // 계산이 가능하다.
+        txtAge.text = "(${2023-stdData.birthYear+1}세)"
 
         return row
     }
--- app/src/main/java/com/example/listview_20231029/datas/StudnetData.kt
+++ app/src/main/java/com/example/listview_20231029/datas/StudnetData.kt
@@ -1,6 +1,9 @@
 package com.example.listview_20231029.datas
 // 필요한 데이터 클래스는 별도 클래스를 만들어 관리.
 
-class StudnetData(name: String, birthYear: Int, phoneNum: String) {
+class StudnetData(
+    val name: String,
+    val birthYear: Int,
+    val phoneNum: String) {
     // stundentData의 생성자 변경 , 이름 , 출생년도 , 전화번호를 넣어서 생성하도록 변경
 }
