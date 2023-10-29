b82c7f51e0bbe9c885d2715c48e6188b08be8548
mnyu123 <whdudwo5582@gmail.com>
2023-10-29 11:19:51 +0900

    학생 정보 데이터 생성자 작성

---
+++ app/src/main/java/com/example/listview_20231029/datas/StudnetData.kt
@@ -0,0 +1,6 @@
+package com.example.listview_20231029.datas
+// 필요한 데이터 클래스는 별도 클래스를 만들어 관리.
+
+class StudnetData(name: String, birthYear: Int, phoneNum: String) {
+    // stundentData의 생성자 변경 , 이름 , 출생년도 , 전화번호를 넣어서 생성하도록 변경
+}
