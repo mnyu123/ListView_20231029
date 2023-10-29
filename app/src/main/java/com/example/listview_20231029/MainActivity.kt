3c6de6018617a3031dd435bdcfa6beb5fa5523ef
mnyu123 <whdudwo5582@gmail.com>
2023-10-29 11:06:44 +0900

    리스트뷰 레이아웃 생성

---
+++ app/src/main/java/com/example/listview_20231029/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.listview_20231029
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.listview_20231029.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+    }
+}
